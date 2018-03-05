2186b5d5a13cc24ca5bd63b9fecdc7beab68cc7a
Phillip <phillipables1@gmail.com>
2018-03-04 21:56:32 -0500

    instantiate our buttons and imageviews in main class

--- app/src/main/java/com/example/techtron/sexherpieshand/MainActivity.java
+++ app/src/main/java/com/example/techtron/sexherpieshand/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.techtron.sexherpieshand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_sex, btn_herpies, btn_hand;
+    ImageView iv_cpu, iv_me;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

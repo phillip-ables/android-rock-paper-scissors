0889573a3a9b4f4d7c193ef32f68ded277d74bfd
Phillip <phillipables1@gmail.com>
2018-03-04 22:16:23 -0500

    onclick set my choice string for clicked value and call calculate method

--- app/src/main/java/com/example/techtron/sexherpieshand/MainActivity.java
+++ app/src/main/java/com/example/techtron/sexherpieshand/MainActivity.java
@@ -7,10 +7,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn_sex, btn_herpies, btn_hand;
     ImageView iv_cpu, iv_me;
+
+    String myChoice, cpuChoice;
+
+    Random r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,25 +30,31 @@ public class MainActivity extends AppCompatActivity {
         btn_herpies = (Button) findViewById(R.id.btn_herpies);
         btn_hand = (Button) findViewById(R.id.btn_hand);
 
+        r = new Random();
+
         btn_sex.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                myChoice = "sex";
+                calculate();
             }
         });
         btn_herpies.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                myChoice = "herpies";
+                calculate();
             }
         });
         btn_hand.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                myChoice = "hand";
+                calculate();
             }
-        });
-
+        })
+    }
+    public void calculate() {
 
     }
 }
